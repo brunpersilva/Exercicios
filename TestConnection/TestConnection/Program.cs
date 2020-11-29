c008188d7ec6e9f66da6217267d7b86d82dec8d1
henriquerbf <henriquerbf@hotmail.com>
2020-11-29 05:04:26 -0300

    TestConnection de nada
    Conexao com banco e insert

---
+++ TestConnection/TestConnection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TestConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connstring = "Data Source=ares\\sqlexpress;Initial Catalog=exercicio;Integrated Security=True";
+
+            SqlConnection qry = new SqlConnection(connstring);
+
+            qry.Open();
+            string insert = "insert cliente (nome, idade, sexo) values ('Bruno', 25, 'M')";
+
+            SqlCommand cmd = new SqlCommand(insert, qry);
+            
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
